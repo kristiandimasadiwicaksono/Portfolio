d7e27686a5a42c6e65f3263e010dc0d4efe829f1
Kristian Dimas Adi Wicaksono <kristiandimasadiwicaksono@gmail.com>
2025-07-08 00:25:51 +0700

    update cv

--- src/partials/cv.js
+++ src/partials/cv.js
@@ -3,7 +3,7 @@ import React from "react";
 const SeeCVButton = () => {
   return (
     <div class="flex justify-center">
-        <a href="https://drive.google.com/file/d/1KMruGDJHijasfpoq0lmhQKob12inFMBv/view?usp=sharing" className="inline-block px-2 py-2 mt-4 text-white bg-blue-600 rounded-lg shadow hover:bg-blue-700 transition duration-300">
+        <a href="https://drive.google.com/file/d/1XYV08Zir_WHL4Tzi33B7kTaNPLUgEHsC/view?usp=sharing" className="inline-block px-2 py-2 mt-4 text-white bg-blue-600 rounded-lg shadow hover:bg-blue-700 transition duration-300">
         See CV
         </a>
     </div>
